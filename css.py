a8c54f72d19e8cfff82f2e9dc967485fcd69a6fd
bobeeq <bobeeq@gmail.com>
2022-06-12 18:33:58 +0200

    Skrypt Python do aktualizacji css

--- css.py
+++ css.py
@@ -1,18 +1,31 @@
 import re
+import shutil
 
 mainFileName = "tomczukToolKit.js"
 try:
-    css = open("style.css", "r")
-    cssText = css.read()
-    js = open(mainFileName, "r+", encoding = 'utf-8')
-    jsText = js.read()
-    newJs = re.sub(r"(customUpdateableCSS\s*\=\s*`)([^`]+)(`)", r"\1\2\3", jsText)
-    toSave = open('nowy.js', "w");
-    toSave.write(newJs)
+    shutil.copyfile('tomczukToolKit.js', mainFileName)
+    cssFile = open("style.css", "r", encoding = 'utf-8')
+    cssText = cssFile.read()
+
+    jsFile = open(mainFileName, "r", encoding = 'utf-8')
+    jsText = jsFile.read()
+    jsFile.close()
+
+    bakFile = open(mainFileName + '.bak', 'w', encoding = 'utf-8')
+    bakFile.write(jsText)
+    print('Backup utworzony: ' + mainFileName + '.bak')
+    
+    newJs = re.sub(
+        r"(customUpdateableCSS\s*\=\s*`)([^`]+)(`)",
+        r"\1" + cssText + r"\3",
+        jsText
+    )
+    jsFile = open(mainFileName, 'w', encoding = 'utf-8')
+    jsFile.write(newJs)
 except BaseException as err:
     print("Wystąpił błąd.")
     print(err)
 finally:
-    css.close()
-    js.close()
+    cssFile.close()
+    jsFile.close()
     print('Plik ' + mainFileName + ' został podmieniony.')
